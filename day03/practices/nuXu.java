9a76f69ec0f68b461655a2102fda10634ee24de4
MorningStar-Inspiron <1537702656@qq.com>
2022-07-27 20:46:55 +0800

    day 04

---
+++ day03/practices/nuXu.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class nuXu{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int jl=sc.nextInt();
+		if(jl>2){
+			System.out.println("Response");
+		}
+		else{
+			System.out.println("No Response");
+		}
+	}
+}
---
+++ day03/practices/reward.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class reward{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int mc = sc.nextInt();
+		if(mc==1){
+			System.out.println("get girlfriend");
+		}
+	}
+}
